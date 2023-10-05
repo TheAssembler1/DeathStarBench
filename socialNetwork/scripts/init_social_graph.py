72eda4dbecb4848f2fef284d6261b253c2078fce
vm_test <lewisnoah592@gmail.com>
2023-10-05 20:42:13 +0000

    update nginx config

--- socialNetwork/scripts/init_social_graph.py
+++ socialNetwork/scripts/init_social_graph.py
@@ -134,7 +134,7 @@ if __name__ == '__main__':
   parser.add_argument(
       '--graph', help='Graph name. (`socfb-Reed98`, `ego-twitter`, or `soc-twitter-follows-mun`)', default='socfb-Reed98')
   parser.add_argument(
-      '--ip', help='IP address of socialNetwork NGINX web server. ', default='10.100.205.74')
+      '--ip', help='IP address of socialNetwork NGINX web server. ', default='10.103.120.71')
   parser.add_argument(
       '--port', help='IP port of socialNetwork NGINX web server.', default=8080)
   parser.add_argument('--compose', action='store_true',
